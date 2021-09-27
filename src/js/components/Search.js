e14b71899c8034cf8769f8a9aecc4d23ed278ca6
Shin-Taro <sh.takahashi.113@gmail.com>
2021-09-28 01:13:35 +0900

    [update]add functions
    テキストフィールドのvalueをstateで保持、propsで受け取るonSubmit関数を設定

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Search = (props) => {
-  const blockName = "search"
+  const [value, setValue] = useState("");
+  const blockName = "search";
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setValue(value);
+  };
+
   return(
-    <form className={blockName}>
-      <input className={`${blockName}__text`} type="text" />
+    <form className={blockName} onSubmit={(e) => props.onSubmit(e)}>
+      <input className={`${blockName}__text`} type="text" value={value} onChange={(e) => handleChange(e)}/>
       <button className={`${blockName}__button`} type="submit">search</button>
     </form>
   );
