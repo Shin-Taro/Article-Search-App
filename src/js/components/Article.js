852ad9c267e69569601289121d82bfe0f9dc1f17
Shin-Taro <sh.takahashi.113@gmail.com>
2021-09-28 13:16:23 +0900

    [update]connect with Qiita api
    qiita apiで検索キーワードから投稿を取得する通信を実装

--- src/js/components/Article.js
+++ src/js/components/Article.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Article = (props) => {
+const Article = ({article}) => {
   const blockName = "article";
-
   return(
     <div className={blockName}>
-      
+      <h1 className={`${blockName}__title`}>{article.title}</h1>
+      <p>{article.user.name}</p>
     </div>
   );
 };
