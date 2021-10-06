e18f72fafa2fd61c694a3498db8338065907c7f7
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-06 14:37:12 +0900

    [add]install corejs3 related modules
    corejs3のpolify関連のインストール

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,13 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
+              presets: [
+                "@babel/preset-env", 
+                "@babel/preset-react", 
+              ],
+              plugins: [
+                ['@babel/plugin-transform-runtime', {corejs: 3}],
+              ],
             },
           },
         ],
