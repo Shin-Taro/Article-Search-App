cf0f4ef2d3e2638bb8a97560e8a8430861785f24
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-07 12:48:17 +0900

    [add]import and provide useData
    useDocumentDataでユーザーのドキュメントを取得しvaluとして共有

--- src/js/context/UserContext.js
+++ src/js/context/UserContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext } from "react";
 import { useAuthContext } from "./AuthContext";
-import { useDocOnce } from "../firebase";
+import { getUser, useDocData } from "../firebase";
 import { Redirect } from 'react-router-dom';
 
 const UserContext = createContext();
@@ -9,19 +9,18 @@ export const useUserContext = () => useContext(UserContext);
 
 export const UserProvider = ({children}) => {
   const { user } = useAuthContext();
-  
+
   if(!user){
     return <Redirect to="/signin" />;
   }else{
-    const [userDoc] = useDocOnce("users", user.uid);
-    console.log(userDoc);
-    const value = {userDoc};
-  
+    const docRef = getUser(user);
+    const [userData, loading, error] = useDocData(docRef);
+    const value = {userData, loading, error};
+
     return(
       <UserContext.Provider value={value}>
         {children}
       </UserContext.Provider>
     );
   }
-
 };
