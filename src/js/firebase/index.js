09a480c58a25eaae89da097c5ba91f28b923037a
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-07 12:47:17 +0900

    [add]add getUser function
    getUser関数でユーザーのドキュメントがなければ初期データを作成

--- src/js/firebase/index.js
+++ src/js/firebase/index.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 // import { initializeApp } from 'firebase/app';
-// import { doc, getFirestore } from'firebase/firestore';
+// import { doc, setDoc } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useDocumentOnce } from 'react-firebase-hooks/firestore';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 import config from './config';
 
 // react-firebase-hooksがv9に対応していないためfirestore関連の記述はv8準拠 //
@@ -21,9 +21,7 @@ export const signIn = () => {
 };
 
 export const logOut = () => {
-  signOut(auth).then(() => {
-    console.log("logOut!!");
-  }).catch((error) => {
+  signOut(auth).catch((error) => {
     console.log(error);
   });
 };
@@ -32,7 +30,38 @@ export const useAuth = () => {
   return useAuthState(auth);
 };
 
-export const useDocOnce = (col, document) => {
-  const docRef = db.collection(col).doc(document);
-  return useDocumentOnce(docRef);
-}
+export const getUser = (user) => {
+  const docRef = db.collection("users").doc(user.uid);
+  docRef.get().then((doc) => {
+    if(!doc.exists){
+      docRef.set({
+        name: user.displayName
+      }).then(() => {
+        docRef.collection("presets").doc("news").set({
+          name: "新着",
+          value: "created%3A%3E2021-08-01",
+          isActive: true,
+        });
+      }).then(() => {
+        docRef.collection("presets").doc("topics").set({
+          name: "人気",
+          value: "created%3A%3E2021-08-01+stocks%3A%3E20",
+          isActive: false,
+        });
+      }).then(() => {
+        docRef.collection("presets").doc("legends").set({
+          name: "殿堂入り",
+          value: "stocks%3A%3E5000",
+          isActive: false,
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  });
+  return docRef;
+};
+
+export const useDocData = (ref) => {
+  return useDocumentData(ref);
+};
