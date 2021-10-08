8890177fc1bb6207323f14cdaca83413751a03bf
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-08 14:04:27 +0900

    [update]create form details
    formの部品を作成、textの値はステートで管理

--- src/js/components/PresetsForm.js
+++ src/js/components/PresetsForm.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PresetsForm = () => {
+  const blockName = "presetsForm";
+  const [values, setValues] = useState({
+    id:"",
+    name:"",
+    query:"",
+  });
+
+  const handleOnChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    setValues({...values, [name]:value})
+  };
+
   return(
-    <div >
-      presets form
-    </div>
+    <form className={blockName}>
+      <p>プリセットの名前</p>
+      <input className={`${blockName}__text`} 
+       type="text"
+       name="name"
+       value={values.name}
+       placeholder="name"
+       onChange={(e) => handleOnChange(e)} />
+
+      <p>検索値</p>
+      <input className={`${blockName}__text`}
+      type="text"
+      name="query"
+      value={values.query}
+      placeholder="query"
+      onChange={(e) => handleOnChange(e)} />
+
+      <button className={`${blockName}__btn`} type="submit">作成</button>
+    </form>
   );
 };
 
