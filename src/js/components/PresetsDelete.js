0738417e81a47576a5cd0cd31b9300af73f48cd9
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-08 21:58:45 +0900

    [add]create deletePresets form
    チェックボックスと削除ボタン実装

--- src/js/components/PresetsDelete.js
+++ src/js/components/PresetsDelete.js
@@ -1,35 +1,45 @@
 import React, { useState } from "react";
 import { useUserContext } from "../context/UserContext";
 import { useAuthContext } from "../context/AuthContext";
+import { deletePresets } from "../firebase";
 
 const PresetsDelete = () => {
   const blockName = "presetsDelete";
   const {user} = useAuthContext();
   const {presets} = useUserContext();
-  const defList = presets.map((item) => {
-    return(
-      {
-        id: item.id,
-        name: item.name,
-        selected: false,
-      }
-    );
-  });
-  const [list, setList] = useState(defList);
+  const [list, setList] = useState([]);
+
+  const handleOnCheck = (e) => {
+    const id = e.currentTarget.dataset.id;
+    const tempList = list.slice();
+
+    if(tempList.includes(id)){
+      const newList = tempList.filter(item => item != id);
+      setList(newList);
+    }else{
+      tempList.push(id);
+      setList(tempList);
+    }
+  };
+
+  const handleOnClick = () => {
+    deletePresets(user, list);
+    setList([]);
+  };
 
   const renderList = () => {
-    const presetsList = list.map((item) => {
+    const presetsList = presets.map((item) => {
       return(
-      <li key={item.id} className={`${blockName}__item`}>
-        <button 
-          className={`${blockName}__btn`} 
-          type="button"
-          data-id={item.id}
-          data-selected={item.selected}
-        >
+        <label key={item.id} className={`${blockName}__item`}>
+          <input
+            className={`${blockName}__checkBox`} 
+            type="checkbox"
+            data-id={item.id}
+            defaultChecked={list.includes(item.id)}
+            onClick={(e) => handleOnCheck(e)}
+          />
           {item.name}
-        </button>
-      </li>
+        </label>
       );
     });
     return presetsList;
@@ -38,10 +48,8 @@ const PresetsDelete = () => {
   return(
     <div className={blockName}>
       <h1 className={`${blockName}__title`}>プリセットの削除</h1>
-      <ul className={`${blockName}__list`}>
-        {renderList()}
-      </ul>
-      <button className={`${blockName}__btn`} type="button">削除</button>
+      {renderList()}
+      <button className={`${blockName}__btn`} type="button" onClick={() => handleOnClick()}>削除</button>
     </div>
   );
 };
