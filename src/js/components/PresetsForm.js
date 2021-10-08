c667bef8b09d30b677e08bb0ee539eb59bd1161c
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-08 14:45:08 +0900

    [add]add functions for posting new preset
    formに入力された値をチェックしてpreset情報を送信する

--- src/js/components/PresetsForm.js
+++ src/js/components/PresetsForm.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { addPreset } from "../firebase";
 
 const PresetsForm = () => {
   const blockName = "presetsForm";
-  const [values, setValues] = useState({
-    id:"",
-    name:"",
-    query:"",
-  });
+  const [values, setValues] = useState({id:"", name:"", query:""});
+  const [message, setMessage] = useState("値を入力してください");
+  const {user} = useAuthContext();
 
   const handleOnChange = (e) => {
     const target = e.target;
@@ -15,8 +15,41 @@ const PresetsForm = () => {
     setValues({...values, [name]:value})
   };
 
+  const checkValues = () => {
+    const name = values.name;
+    const query = values.query;
+    const nameReg = /^.{1,10}$/;
+    const queryReg = /^.{1,1000}$/;
+    const verifiedName = nameReg.test(name);
+    const verifiedQuery = queryReg.test(query);
+
+    if(!name || !query){
+      return "項目が入力されていません";
+    }else if(!verifiedName || !verifiedQuery){
+      return "不正な入力です";
+    }else{
+      return true;
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const result = checkValues();
+    if(typeof(result) === "string"){
+      setMessage(result);
+    }else if(!result){
+      console.log("false");
+    }else{
+      const escapedValue = encodeURIComponent(values.query);
+      addPreset(user, values.name, escapedValue);
+      setValues({id:"", name:"", query:""});
+      setMessage("success!!");
+    }
+  }
+
   return(
-    <form className={blockName}>
+    <form className={blockName} onSubmit={(e) => handleOnSubmit(e)}>
+      <p>{message}</p>
       <p>プリセットの名前</p>
       <input className={`${blockName}__text`} 
        type="text"
