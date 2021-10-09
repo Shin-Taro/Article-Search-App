3fbaf97e7ca4ef2fbe75d6e635524ec2845b55de
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-09 10:49:10 +0900

    [add]add userName on header
    Headerにログイン中のユーザー名を表示

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,10 +2,12 @@ import React, {useState} from "react";
 import Modal from "./Modal";
 import Console from "./Console";
 import SignOut from "./SignOut";
+import { useAuthContext } from "../context/AuthContext";
 
 const Header = () => {
   const blockName = "header";
   const [show, setShow] = useState(false);
+  const {user} = useAuthContext();
 
   const toggleModal = () => {
     setShow(!show);
@@ -14,6 +16,7 @@ const Header = () => {
   return(
     <header className={blockName}>
       <h1 className={`${blockName}__title`}>Qiita Customized App</h1>
+      <p className={`${blockName}__user`}>ユーザー名：{user.displayName}</p>
       <div className={`${blockName}__btns`}>
         <SignOut />
         <button className={`${blockName}__modal`}
