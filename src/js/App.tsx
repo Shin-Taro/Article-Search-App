5bcf3921048f44fb53d88e6eb13c5ae279cf7f86
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-13 12:30:43 +0900

    [update]change way of writing App.js
    returnのみのコンポーネントのため記述スタイルを簡略化

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -5,17 +5,15 @@ import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter, Route } from 'react-router-dom';
 import { UserProvider } from "./context/UserContext";
 
-const App = () => {
-  return(
-    <AuthProvider>
-      <BrowserRouter>
-        <UserProvider>
-          <Route exact path="/" component={ArticleSearcher} />
-        </UserProvider>
-        <Route path="/signin" component={SignIn} />
-      </BrowserRouter>
-    </AuthProvider> 
-  );
-};
+const App = () => (
+  <AuthProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <Route exact path="/" component={ArticleSearcher} />
+      </UserProvider>
+      <Route path="/signin" component={SignIn} />
+    </BrowserRouter>
+  </AuthProvider> 
+);
 
 export default App;
