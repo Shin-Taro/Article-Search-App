a7fa41d13a9a0d70c22b86f80bf9f5f1d3f44b4b
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-14 23:11:27 +0900

    [update]render Router component
    react-router-domのルーティングを廃止し、Routerコンポーネントをレンダー

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import ArticleSearcher from "./components/ArticleSearcher";
-import SignIn from "./components/SignIn";
 import { AuthProvider } from "./context/AuthContext";
-import { BrowserRouter, Route } from 'react-router-dom';
-import { UserProvider } from "./context/UserContext";
+import Router from "./components/Router";
 
 const App = () => (
   <AuthProvider>
-    <BrowserRouter>
-      <UserProvider>
-        <Route exact path="/" component={ArticleSearcher} />
-      </UserProvider>
-      <Route path="/signin" component={SignIn} />
-    </BrowserRouter>
+    <Router/>
   </AuthProvider> 
 );
 
