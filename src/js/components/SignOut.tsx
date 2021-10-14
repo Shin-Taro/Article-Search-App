519c87b995296ef84e25db011bb82550a1a75ce4
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-14 16:07:07 +0900

    [update]update SignOut with tsx
    SignOutコンポーネントをTypeScriptでリファクタリング

--- src/js/components/SignOut.tsx
+++ src/js/components/SignOut.tsx
@@ -3,14 +3,14 @@ import { logOut } from "../firebase";
 import { useHistory } from 'react-router-dom';
 
 const SignOut = () => {
-  const blockName = "signOut";
+  const blockName:string = "signOut";
   const history = useHistory();
 
-  const handleOnClick = (e) => {
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>):void => {
     e.preventDefault();
     logOut();
     history.push("/signin");
-  }
+  };
 
   return(
     <div className={blockName}>
