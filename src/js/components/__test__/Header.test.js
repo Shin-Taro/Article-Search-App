3c12c24823a31d185174dfe6beedb56a676ab3da
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-23 00:05:17 +0900

    [fix]remove imported Modal
    Header.test内のModalコンポーネントのimportは不要のため削除

--- src/js/components/__test__/Header.test.js
+++ src/js/components/__test__/Header.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Header from "../Header";
-import Modal from "../Modal";
 
 jest.mock("../../context/AuthContext", () => ({
   useAuthContext: jest.fn(() => ({user:{displayName:"fakename"}})),
